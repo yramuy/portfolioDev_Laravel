b2730eff8517c6a2f46e2cdea407ff3081427ddc
yramuy <ramuy77290@gmail.com>
2024-07-25 12:49:01 +0530

    push changes
    Signed-off-by: yramuy <ramuy77290@gmail.com>

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Models\About;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
@@ -19,5 +21,35 @@ class ApiController extends Controller
             'skills' => $skills
         ];
         return response()->json($response);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $input = $request->validated();
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::guard('admin')->attempt($credentials)) {
+
+            $user = Auth::guard('admin')->user();
+            if ($user->role == 1) {
+                $roleName = 'Admin';
+            } else if ($user->role == 2) {
+                $roleName = 'Finance';
+            } else {
+                $roleName = 'Administrative';
+            }
+
+            $userData = array(
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'role_id' => $user->role,
+                'role_name' => $roleName
+            );
+
+            return response()->json(array('status' => 200, 'message' => 'Logged in successfully', 'userDetails' => $userData));
+        } else {
+            return response()->json(array('status' => 404, 'message' => 'Incorrect Username or Password', 'userDetails' => []));
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('profileData', [ApiController::class, 'ProfileData'])->name('profileData');
+Route::post('login', [ApiController::class, 'login'])->name('login');
